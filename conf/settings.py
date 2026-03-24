b89adca6946cbaafdd70bd67315df83c575b4848
Azizbek Qochqorov <qazizbek24@gmail.com>
2026-03-24 18:12:27 +0500

    .

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2sxqff+hxpoki*axf$zg@fis@i7p+wm_8s_@3c28$@#pt!cn$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
