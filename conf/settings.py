e838baffa1c2ef31637f1926bb172da6d6917578
Azizbek Qochqorov <qazizbek24@gmail.com>
2026-03-24 18:29:53 +0500

    .

--- conf/settings.py
+++ conf/settings.py
@@ -129,4 +129,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
